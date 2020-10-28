f2ec83c11b7b421abe4c9e1030aeb78148bd0dec
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-28 16:48:30 -0400

    Fixed. Broken extern variable problem was caused by not reading in the entire kernel.bin into memory. We only read in 15 sectors from the floppy drive. Our kernel.bin has grown into 20 sectors.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -62,7 +62,7 @@ int main(void) {
     //unsigned long long t = 0;
     //print_byteh((unsigned char) sizeof(t));
     unsigned char *t = (unsigned char *) &idt[0];
-    t += 0x200 - 0x1000;
+    //t += 0x200 - 0x1000;
     unsigned int taddr = (unsigned int) t;
 
     for (int i = 7; i >= 0; i--) {
