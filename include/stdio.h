82ab27d0cb9c7e170382d1058cfceee414c8e77b
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-11-21 01:57:41 -0500

    Create stdio.h.

---
+++ include/stdio.h
@@ -0,0 +1,13 @@
+#ifndef __STDIO_H__
+#define __STDIO_H__
+
+/*!
+    @defined    NULL
+
+    @discussion An alias for 0. Typically used with pointers. "The symbolic
+    constant NULL is often used in place of zero, as a mnemonic to indicate more
+    clearly that this is a special value for a pointer."
+*/
+#define NULL (0)
+
+#endif
