8919de29b2710fce16b165f3edb3c98ae6e384d9
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-11-21 04:11:26 -0500

    More standard headers.

---
+++ include/stddef.h
@@ -0,0 +1,14 @@
+/*!
+    @header Standard C Header
+*/
+#ifndef __STDDEF_H__
+#define __STDDEF_H__
+
+/*!
+    @typedef size_t
+
+    @discussion The type returned by sizeof. @TODO Double check this at runtime.
+*/
+typedef unsigned int size_t
+
+#endif
