0003db9fe44230997b1417b4bca691eaa394e042
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-11-22 04:04:06 -0500

    Headers progress.

--- include/assert.c
+++ include/assert.c
@@ -1,7 +1,8 @@
 /*!
     @function dead_loop
 
-    @discussion Infinite loop.
+    @discussion Infinite loop. @TODO The standard assert macro calls abort()
+    instead of looping.
 
 */
 void dead_loop(void) {
@@ -13,16 +14,25 @@ void dead_loop(void) {
     @function print_assert
 
     @discussion Prints a message associated with the assert macro. @doc
-    [assert.h].
+    [assert.h on your local machine](include/assert.h)
+    @doc [man page for assert](man assert). @TODO This is a temporary
+    approximation to what the standard assert macro prints. Print should be
+    replaced by printf().
+
+    @param    e    A string, the expression that was evaluated as false by the
+                   assert macro.
+
+    @param    f    A string, the name of the file in which the assert macro was
+                   called.
+
+    @param    l    The line number at which the assert macro was called.
 
 */
 void print_assert(char *e, char *f, int l) {
     print(f);
     print(":");
-    print_uint32h(l);
+    print_uint32h(l); // @TODO Print in decimal.
     print(": failed assertion `");
     print(e);
     print("'\n");
 }
-
-
--- include/assert.h
+++ include/assert.h
@@ -1,13 +1,13 @@
 /*!
     @header Standard C Header
 */
+
 #ifndef __ASSERT_H__
 #define __ASSERT_H__
 
-/*! See .c */
-void dead_loop(void);
-/*! See .c */
-void print_assert(char *e, char *f, int l);
+#ifdef NDEBUG
+#define assert(e)    ((void)0) // Implements disabling asserts. See man.
+#else
 
 /*!
     @defined assert(e)
@@ -26,4 +26,51 @@ void print_assert(char *e, char *f, int l);
 #define __assert(e, file, line) \
    ( (void) (print_assert(e, file, line), dead_loop() ) )
 
+/*! See .c */
+void dead_loop(void);
+
+/*! See .c */
+void print_assert(char *e, char *f, int l);
+
 #endif
+
+#endif
+/*******************************************************************************
+ASSERT(3)                BSD Library Functions Manual                ASSERT(3)
+
+NAME
+     assert -- expression verification macro
+
+SYNOPSIS
+     #include <assert.h>
+
+     assert(`expression`);
+
+DESCRIPTION
+     The assert() macro tests the given `expression` and if it is false, the
+     calling process is terminated.  A diagnostic message is written to `stderr`
+     and the abort(3) function is called, effectively terminating the program.
+
+     If `expression` is true, the assert() macro does nothing.
+
+     The assert() macro may be removed at compile time with the cc(1) option
+     -DNDEBUG.
+
+DIAGNOSTICS
+     The following diagnostic message is written to `stderr` if `expression` is
+     false:
+
+           "assertion \"%s\" failed: file \"%s\", line %d\n", \
+                               "expression", __FILE__, __LINE__);
+
+SEE ALSO
+     cc(1), abort(3)
+
+STANDARDS
+     The assert() macro conforms to ANSI X3.159-1989 (``ANSI C89'').
+
+HISTORY
+     A assert macro appeared in Version 6 AT&T UNIX.
+
+BSD                              June 9, 1993                              BSD
+*******************************************************************************/
