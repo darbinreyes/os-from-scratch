85bd41c7c91688c02dfddc802e4ee4951e9db44e
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-12-11 01:57:50 -0500

    Start moving the PIC code into its own file.

---
+++ kernel/i8259a_pic.c
@@ -0,0 +1,9 @@
+/*!
+    @header Initialization and configuration of the Intel 8259A PIC.
+    Intel 8259A Programmable Interrupt Controller (PIC) is one of the chips
+    that can be used for delivering interrupts to an Intel CPU.
+    @doc [i386 Interrupt Handling w/ Programmable Interrupt Controller]
+         (./docs/interrupts/sigops_i386_Interrupt_Handling.pdf)
+    @doc [Intel 8259A chip datasheet - Details on control words]
+         (./docs/interrupts/intel-8259a-pic.pdf)
+*/
